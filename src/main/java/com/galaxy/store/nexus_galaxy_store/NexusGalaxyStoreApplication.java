7394e6990c2e49a58388dd4ed9e2fe0762eca811
farhaddubey <farhaddubey003@gmail.com>
2026-05-24 20:19:22 +0530

    nexus galaxy store being set

---
+++ src/main/java/com/galaxy/store/nexus_galaxy_store/NexusGalaxyStoreApplication.java
@@ -0,0 +1,13 @@
+package com.galaxy.store.nexus_galaxy_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusGalaxyStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexusGalaxyStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/galaxy/store/nexus_galaxy_store/NexusGalaxyStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galaxy.store.nexus_galaxy_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexusGalaxyStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
